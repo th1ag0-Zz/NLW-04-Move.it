1027e53762317a95b520b2627a53f165c688ba16
Thiago <thiagoF89.tf@gmail.com>
2021-02-22 20:46:22 -0300

    dia 1 finalizado

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,17 @@
+export function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: '60%' }} />
+        <span
+          className="current-experience"
+          style={{ left: '60%' }}
+        >
+          360 xp
+        </span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  )
+}
