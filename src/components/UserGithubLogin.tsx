abcec7fdb81958dddddc3d9c91c8ee66023c179b
Thiago <thiagoF89.tf@gmail.com>
2021-03-05 13:09:50 -0300

    :tada: github login page finalized :tada:

--- src/components/UserGithubLogin.tsx
+++ src/components/UserGithubLogin.tsx
@@ -3,7 +3,20 @@ import styles from '../styles/components/UserGithubLogin.module.css';
 export function UserGithubLogin() {
   return (
     <div className={styles.container}>
-      
+      <div className={styles.box_login}>
+        <img src="/logo-full-white.svg" alt="Moveit"/>
+        <h1>Bem-vindo</h1>
+        <p>
+          <img src="/icons/github.svg" alt="github"/>
+          Faça login com seu Github <br/> para começar
+        </p>
+        <form className={styles.formContainer} action="">
+          <input placeholder="Digite seu username" required type="text"/>
+          <button type="submit">
+            <img src="/icons/arrow_right.svg" alt=""/>
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
